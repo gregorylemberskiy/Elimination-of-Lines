af49f1e6197a9cad78aff7b0e154a493aa7de451
Gregory Lemberskiy <gregorylemberskiy@gmail.com>
2012-04-03 01:16:41 -0400

    Added additional functions

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
 """
 * -----------------------------------------------------------------------------
 * MODULE: elmpy
+*
+* Author: Gregory Lemberskiy
 * -----------------------------------------------------------------------------
 """
 
 import hough
 import Optim 
+import Histeq
+import Highpass
+import gl_imshow
